d67226578ca8ed7824e7b243afc753a49477ca0c
fivos <fivos.vardis@gmail.com>
2022-05-10 19:58:23 +0300

    settings commit

--- assetLocator/settings.py
+++ assetLocator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3*yt2#zz4r4+3p$+&*)g51b0+=e5pfs7l79oim!_3ehwlm+8sr'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
